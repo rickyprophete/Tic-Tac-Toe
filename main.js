0b456d81b9e1eeecfbbab940a612ccacd80bcaa5
rickyprophete <rickyprophete@gmail.com>
2021-05-13 18:20:51 -0400

    player vs player logic implemented

--- main.js
+++ main.js
@@ -2,34 +2,51 @@
 //getting interactive elements the butttons in header and divs in game-cell
 const statusHead = document.querySelectorAll('.header-button');
 const gameCells = document.querySelectorAll('.game-cell');
+const gameGrid = document.querySelector('.game-grid');
 const footer = document.querySelector('.footer button');
 
 
 //game variables
 let xIsNext = true;
+let isLive = true;
 let xWins = 0;
 let oWins = 0;
-
+let winner = null;
 
 //functions for eventhandlers
 const move = (e) =>{
-  if(e.target.innerHTML==='X' || e.target.innerHTML==='O'){
+  if(e.target.innerHTML==='X' || e.target.innerHTML==='O' || !isLive){
     return;
   }
 
   if(xIsNext){
     e.target.innerHTML= 'X';
-    xIsNext = !xIsNext;
+    if(isWinner('X')){
+      statusHead[0].innerHTML = `X  :   ${xWins}`;
+      statusHead[1].innerHTML = 'X Wins';
+
+
+    }
   }else{
     e.target.innerHTML= 'O';
-    xIsNext = !xIsNext;
+    if(isWinner('O')){
+      statusHead[0].innerHTML = 'O Wins';
+      statusHead[1].innerHTML = `O  :   ${oWins}`;
+    }
   }
+  if(isLive)
+    xIsNext=!xIsNext;
+
 }
 
 const restart = (e) =>{
   for(const cellDiv of gameCells){
     cellDiv.innerHTML = '';
   }
+  xIsNext=true;
+  isLive = true;
+  statusHead[0].innerHTML = `X  :   ${xWins}`;
+  statusHead[1].innerHTML = `O  :   ${oWins}`;
 }
 
 //eventhandlers
@@ -48,45 +65,56 @@ const isWinner =(p)=> ////if p wins return true
     for (let i = 0; i < N; i = i + 3)
     {
         if (
-            boardCopy[i] == p &&
-            boardCopy[i + 1] == p &&
-            boardCopy[i + 2] == p
+            boardCopy[i].innerHTML === p &&
+            boardCopy[i + 1].innerHTML === p &&
+            boardCopy[i + 2].innerHTML === p
            )
         {
-            return true;
+          winner=p;
+          winner === 'X' ? xWins++ : oWins++;
+          isLive=false;
+          return true;
         }
     }
     for (let i = 0; i < N - 6; i++)
     {
         if (
-            boardCopy[i] == p &&
-            boardCopy[i + 3] == p &&
-            boardCopy[i + 6] == p
+            boardCopy[i].innerHTML === p &&
+            boardCopy[i + 3].innerHTML === p &&
+            boardCopy[i + 6].innerHTML === p
            )
         {
-            return true;
+            
+          winner=p;
+          winner === 'X' ? xWins++ : oWins++;
+          isLive=false;
+          return true;
         }
     }
     if (
-        boardCopy[0] == p &&
-        boardCopy[4] == p &&
-        boardCopy[8] == p
+        boardCopy[0].innerHTML === p &&
+        boardCopy[4].innerHTML === p &&
+        boardCopy[8].innerHTML === p
        )
     {
-        return true;
+      winner=p;  
+          isLive=false;
+          winner === 'X' ? xWins++ : oWins++;
+          return true;
     }
     if (
-        boardCopy[2] == p &&
-        boardCopy[4] == p &&
-        boardCopy[6] == p
+        boardCopy[2].innerHTML === p &&
+        boardCopy[4].innerHTML === p &&
+        boardCopy[6].innerHTML === p
        )
     {
-        return true;
+      winner=p;
+          winner === 'X' ? xWins++ : oWins++;
+          isLive=false;
+          return true;
     }
     return false;
 }
 
 
 
-statusHead[0].innerHTML = `X  :   ${xWins}`;
-statusHead[1].innerHTML = `O  :   ${oWins}`;
